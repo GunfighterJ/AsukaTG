2938bc2878d2ef6470166c62c7c6438d4f4ffa78
Lee Watson <rev@revthefox.co.uk>
2015-11-30 02:01:19 +0000

    Output true or false for setwebhook

--- webhook.php
+++ webhook.php
@@ -27,7 +27,7 @@ $telegram = new Api($api_key, $async);
 
 if (array_key_exists('setwebhook', $_GET)) {
     $response = $telegram->setWebhook($webhook_url);
-    echo $response->getBody();
+    echo $response;
     return;
 }
 
