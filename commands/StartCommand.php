aba6e346b652480e9e16bba0dfd4b5efd611281d
Lee Watson <rev@revthefox.co.uk>
2015-12-02 17:45:03 +0000

    Move StartCommand to it's own file for dynamic loading

---
+++ commands/StartCommand.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of AsukaTG.
+ *
+ * AsukaTG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AsukaTG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AsukaTG.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Asuka\Commands;
+
+use Telegram\Bot\Commands\Command;
+
+class StartCommand extends Command
+{
+    protected $name = "start";
+    protected $description = "Displays a list of bot commands.";
+
+    public function handle($arguments)
+    {
+        $this->triggerCommand('?');
+    }
+}
