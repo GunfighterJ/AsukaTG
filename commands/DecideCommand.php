f2a1d4fb2f1fc6b74f89af6865ea40dc52740072
Lee Watson <rev@revthefox.co.uk>
2015-12-21 17:03:48 +0000

    Add DecideCommand

---
+++ commands/DecideCommand.php
@@ -0,0 +1,88 @@
+<?php
+/*
+ * This file is part of AsukaTG.
+ *
+ * AsukaTG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AsukaTG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AsukaTG.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Asuka\Commands;
+
+use Telegram\Bot\Commands\Command;
+
+class DecideCommand extends Command
+{
+    const RESULT_NO = 0;
+    const RESULT_MAYBE = 1;
+    const RESULT_YES = 2;
+
+    protected $name = "decide";
+    protected $description = "Decides between a set of choices.";
+
+    protected $resultMap = [
+        self::RESULT_NO    => 'No.',
+        self::RESULT_MAYBE => 'Maybe.',
+        self::RESULT_YES   => 'Yes.'
+    ];
+
+    public function handle($arguments)
+    {
+        if (empty($arguments)) {
+            $response = 'Please supply some choices.' . PHP_EOL;
+            $response .= 'Example: /decide Cookies | Cake';
+            $this->reply($response);
+
+            return;
+        }
+
+        $delim = null;
+        if (str_contains($arguments, 'or')) {
+            $delim = 'or';
+        } elseif (str_contains($arguments, '|')) {
+            $delim = '|';
+        } elseif (str_contains($arguments, ',')) {
+            $delim = ',';
+        }
+
+        $result = mt_rand(self::RESULT_NO, self::RESULT_YES);
+        $yesNoResponse = $this->resultMap[$result];
+
+        if (is_null($delim)) {
+            $this->reply($yesNoResponse);
+
+            return;
+        }
+
+        if (empty(trim(str_replace($delim, '', $arguments)))) {
+            $response = 'Please supply some choices.' . PHP_EOL;
+            $response .= 'Example: /decide Cookies | Cake';
+            $this->reply($response);
+
+            return;
+        }
+
+        $choices = array_map('trim', explode($delim, $arguments));
+        if (count(array_filter($choices)) < 2) {
+            $this->reply($yesNoResponse);
+
+            return;
+        }
+
+        $this->reply($choices[array_rand($choices)]);
+    }
+
+    private function reply($response)
+    {
+        $this->replyWithMessage($response, true, $this->getUpdate()->getMessage()->getMessageId(), null);
+    }
+}
