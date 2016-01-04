345145505032b8b0bd734abed46c54edafae0807
Lee Watson <rev@revthefox.co.uk>
2016-01-04 22:08:01 +0000

    Fixes to import script

--- scripts/update_joehot_quotes.php
+++ scripts/update_joehot_quotes.php
@@ -4,18 +4,18 @@ const QUOTE_DB_SOURCE_URL = 'https://git.yawk.at/?p=jhq-server.git;a=blob_plain;
 
 $dataPath = realpath(__DIR__) . '/../data/';
 $quoteDatabase = $dataPath . 'joehot.db';
+
 $db = new PDO('sqlite:' . $quoteDatabase);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->exec("CREATE TABLE IF NOT EXISTS quotes (id INTEGER PRIMARY KEY AUTOINCREMENT, citation TEXT NOT NULL DEFAULT 'joehot200', source TEXT DEFAULT NULL, quote TEXT NOT NULL)");
+$sth = $db->prepare('INSERT INTO quotes (citation, source, quote) VALUES (:citation, :source, :quote)');
 
 $lines = explode("\n", file_get_contents(QUOTE_DB_SOURCE_URL));
-$lines = array_filter($lines, function($line) {
+$lines = array_filter($lines, function ($line) {
     return strpos($line, '#') != 0;
 });
 
 foreach ($lines as $quote) {
-    $quoteParts = [];
-
     // Parse the quote DB according to the rules defined at https://git.yawk.at/?p=jhq-server.git;a=blob;f=README.md;h=a0894ebf6cd5bd94488bb61c0bf3d5ec54821e61;hb=HEAD
     if (preg_match('/^(.*)::/', $quote, $matches)) {
         $quoteParts['citation'] = trim(rtrim($matches[0], '::'));
@@ -39,7 +39,6 @@ foreach ($lines as $quote) {
     $existing->execute();
     $result = $existing->fetch(PDO::FETCH_OBJ);
 
-    $sth = $db->prepare('INSERT INTO quotes (citation, source, quote) VALUES (:citation, :source, :quote)');
     if (!isset($result->id)) {
         $sth->bindValue(':citation', array_key_exists('citation', $quoteParts) ? $quoteParts['citation'] : 'joehot200');
         $sth->bindValue(':source', array_key_exists('source', $quoteParts) ? $quoteParts['source'] : null);
