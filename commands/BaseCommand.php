75d326bf7d8a52e2fb09d40ff9937aad91b62ca1
Lee Watson <rev@revthefox.co.uk>
2016-01-05 16:37:12 +0000

    PHPdocs

--- commands/BaseCommand.php
+++ commands/BaseCommand.php
@@ -25,6 +25,32 @@ class BaseCommand extends Command
     }
 
     /**
+     * @return null|PDO
+     */
+    public function getDatabase()
+    {
+        if (!$this->database) {
+            if (!file_exists($this->databasePath)) {
+                $this->reply('Bot database doesn\'t exist!');
+
+                return null;
+            }
+
+            try {
+                $this->database = new PDO('sqlite:' . $this->databasePath);
+            } catch (\PDOException $exception) {
+                $this->reply($exception->getMessage());
+
+                return null;
+            }
+
+            $this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+        }
+
+        return $this->database;
+    }
+
+    /**
      * @param $response String to send as a reply
      * @param array $params Extra parameters to sendMessage
      */
@@ -36,24 +62,5 @@ class BaseCommand extends Command
         ], $params);
 
         $this->replyWithMessage($params);
-    }
-
-    public function getDatabase() {
-        if (!$this->database) {
-            if (!file_exists($this->databasePath)) {
-                $this->reply('Bot database doesn\'t exist!');
-                return null;
-            }
-
-            try {
-                $this->database = new PDO('sqlite:' . $this->databasePath);
-            } catch (\PDOException $exception) {
-                $this->reply($exception->getMessage());
-                return null;
-            }
-
-            $this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-        }
-        return $this->database;
     }
 }
