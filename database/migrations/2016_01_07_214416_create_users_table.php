76a88ad0c37be9357f2eab4683bf4fe66aa096e7
Lee Watson <rev@revthefox.co.uk>
2016-01-08 15:49:37 +0000

    Use utf8mb4

--- database/migrations/2016_01_07_214416_create_users_table.php
+++ database/migrations/2016_01_07_214416_create_users_table.php
@@ -14,9 +14,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigInteger('id');
-            $table->string('first_name');
-            $table->string('last_name')->nullable();
-            $table->string('username')->nullable();
+            $table->string('first_name', 191);
+            $table->string('last_name', 191)->nullable();
+            $table->string('username', 191)->nullable();
             $table->timestamp('created_at')->default(app('db')->connection()->raw('CURRENT_TIMESTAMP'));
             $table->primary('id');
         });
