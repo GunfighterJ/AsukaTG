ee1d464cb4230ead32efb71048adb3b4dd1a7cae
Lee Watson <rev@revthefox.co.uk>
2016-03-03 07:07:19 +0000

    derp

--- app/Commands/GoogleCommand.php
+++ app/Commands/GoogleCommand.php
@@ -44,7 +44,7 @@ class GoogleCommand extends BaseCommand
         $this->replyWithChatAction(['action' => Actions::TYPING]);
 
         $query = trim(rawurlencode($arguments));
-        $url = sprintf('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=%s&rsz=4', $query);
+        $url = sprintf('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=%s&rsz=5', $query);
         $body = Helpers::urlGetContents($url);
         $json = json_decode($body);
 
