8e61cdab99f2992989b1f2e08bc5f1bb6d1e43b0
Lee Watson <rev@revthefox.co.uk>
2016-06-23 11:41:58 +0100

    Add whatthecommit alias

--- app/Commands/WtcCommand.php
+++ app/Commands/WtcCommand.php
@@ -9,6 +9,7 @@ class WtcCommand extends BaseCommand
 {
     protected $description = 'Gets a random commit message from whatthecommit.com.';
     protected $name = 'wtc';
+    protected $aliases = ['whatthecommit'];
 
     public function handle($arguments)
     {
@@ -16,7 +17,7 @@ class WtcCommand extends BaseCommand
         $html = Helpers::urlGetContents($url);
 
         $dom = new DOMDocument();
-        if ($dom->loadHTML($html)) {
+        if ($dom->loadHTML($html) && $commitMessage = $dom->getElementById('content')->getElementsByTagName('p')->item(0)->nodeValue) {
             $commitMessage = $dom->getElementById('content')->getElementsByTagName('p')->item(0)->nodeValue;
         } else {
             $this->reply('Failed to load content from whatthecommit.com', ['disable_web_page_preview' => true]);
